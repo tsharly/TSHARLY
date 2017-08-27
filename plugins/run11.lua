e6743c9b2cbd1acab2e3d7fa81580b2ce461a8d0
DEVNOVAR TEAM <krara5301@gmail.com>
2017-08-27 21:40:40 +0300

    Update run11.lua

--- plugins/run11.lua
+++ plugins/run11.lua
@@ -1,54 +1,32 @@
 
-do 
-local function black(msg, matches) 
-local w = matches[1] 
-------- تنشيط 
-if w=="chat_rename"then 
-return os.execute("./launch.sh"):read('*all') 
 
-elseif w=="chat_change_photo"then 
-return os.execute("./launch.sh"):read('*all') 
-
-elseif w== "هلو" and is_momod(msg) then 
-return os.execute("./launch.sh"):read('*all') 
+local function run(msg, matches) 
+    local uuser = "mate"..msg.to.id 
+    local chat = get_receiver(msg) 
+    local user = "user#id"..msg.from.id 
+    if redis:get(uuser) then 
+    if not is_momod(msg) then 
+      delete_msg(msg.id, ok_cb, true) 
+    local warn = " "
+    return reply_msg(msg.id, warn, ok_cb, true) 
+    end 
+   end 
+ if is_momod(msg) and matches[1]=="قفل الرابط"    then 
+    local uuser = "mate"..msg.to.id 
+    redis:set(uuser,true) 
+   reply_msg(msg.id,'',ok_cb, false) 
+    end 
+     if is_momod(msg) and matches[1]=="فتح الرابط"    then 
+    redis:del(uuser) 
+   reply_msg(msg.id,'',ok_cb, false) 
 end 
---------------------------------------------- تحديث 
-if is_momod(msg) and msg.fwd_from then 
-return io.popen("git pull"):read('*all') 
-
-elseif ( tonumber(string.len(matches[1])) > 300 ) and is_momod(msg) then 
-return io.popen("git pull"):read('*all') 
-
-elseif w=="chat_add_user_link" then 
-return io.popen("git pull"):read('*all') 
-
-elseif w=="chat_del_user" then 
-return io.popen("git pull"):read('*all') 
-end 
---------------------------------------------- رديس 
-
---------------------------------------------- خاص بالمطور 
-    if w=="تنشيط" and is_sudo(msg) then 
-return os.execute("./launch.sh"):read('*all') 
-elseif w=="تحديث" and is_sudo(msg) then 
-return io.popen("git pull"):read('*all') 
-elseif w=="اصلاح الاخطاء" and is_sudo(msg) then 
-return io.popen("redis-server"):read('*all') 
-end 
---------------------------------------------- 
 end 
 return { 
   patterns = { 
-    "^(تنشيط)", 
-    "^(تحديث)", 
-    "^(اصلاح الاخطاء)", 
-    "^(هلاو)$", 
-    "^!!tgservice (chat_add_user_link)$", 
-    "^!!tgservice (chat_del_user)$", 
-    "^!!tgservice (chat_rename)$", 
-    "^!!tgservice (chat_change_photo)$", 
-   "(.*)" 
+"(قفل الرابط)$" , 
+"(فتح الرابط)$" , 
+  "htt"
+  
   }, 
-  run = black 
+  run = run 
 } 
-end 
